431208f0baf1c73cdf0046d4f95297181496b7b5
chrishavlin <chris.havlin@gmail.com>
2022-01-26 15:24:47 -0600

    improving the reader test

--- src/yt_napari/_tests/test_reader.py
+++ src/yt_napari/_tests/test_reader.py
@@ -1,12 +1,14 @@
-import numpy as np
-from yt_napari import napari_get_reader
 import json
 
+import pytest
+import yt
+
+from yt_napari import napari_get_reader
 
 # the following should be a valid json, need to add some functionality to
 # test infrastructure for loading an actual dataset... as is, this requires
 # the IsolatedGalaxy file so will only pass locally.
-valid_json_dict = {
+valid_jdict = {
     "$schema": "yt-napari_0.0.1.json",
     "dataset": "IsolatedGalaxy/galaxy0030/galaxy0030",
     "field_type": "enzo",
@@ -19,25 +21,51 @@ valid_json_dict = {
 }
 
 
-# tmp_path is a pytest fixture
-def test_reader(tmp_path):
-    """An example of how you might test your plugin."""
-
+@pytest.fixture
+def json_file_fixture(tmp_path):
     # write some fake data using your supported file format
     json_file = str(tmp_path / "valid_json.json")
-    with open(json_file, 'w') as fp:
-        json.dump(valid_json_dict, fp)
+    with open(json_file, "w") as fp:
+        json.dump(valid_jdict, fp)
+    return json_file
 
 
+def cannot_load_file(dataset_file: str) -> bool:
+    # returns True if yt cannot load the provided file.
+    # There may be a simpler way to handle this, perhaps something in the
+    # testing module in yt.
+    try:
+        _ = yt.load(dataset_file)
+        skip_test = False
+    except FileNotFoundError:
+        skip_test = True
+    return skip_test
+
+
+# tmp_path is a pytest fixture for a temporary directory
+def test_cannot_load_file():
+    assert cannot_load_file("/this/file/does/not/exist")
+
+
+def test_reader_identification(json_file_fixture):
+    """tests that the plugin will succesfully identify a valid json"""
     # try to read it back in
-    reader = napari_get_reader(json_file)
+    reader = napari_get_reader(json_file_fixture)
     assert callable(reader)
 
+
+@pytest.mark.skipif(
+    cannot_load_file(valid_jdict["dataset"]), reason="Cannot find test dataset"
+)
+def test_reader_load(json_file_fixture):
     # make sure we're delivering the right format
-    layer_data_list = reader(json_file)
+
+    # get the reader and then call it with the valid json
+    reader = napari_get_reader(json_file_fixture)
+    layer_data_list = reader(json_file_fixture)
     assert isinstance(layer_data_list, list) and len(layer_data_list) > 0
-    layer_data_tuple = layer_data_list[0]
-    assert isinstance(layer_data_tuple, tuple) and len(layer_data_tuple) > 0
+    layer_tuple = layer_data_list[0]
+    assert isinstance(layer_tuple, tuple) and len(layer_tuple) > 0
 
 
 def test_get_reader_pass():
