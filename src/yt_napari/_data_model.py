37d1e11cacdfdcdfa62964a1cf79ecb368d35c45
chrishavlin <chris.havlin@gmail.com>
2022-01-26 16:59:38 -0600

    test improvements, linting

--- src/yt_napari/_data_model.py
+++ src/yt_napari/_data_model.py
@@ -1,7 +1,9 @@
-from pydantic import BaseModel
-from typing import Optional, Tuple, List, Union
-from pathlib import PosixPath
 import inspect
+from pathlib import PosixPath
+from typing import Optional, Tuple, Union
+
+from pydantic import BaseModel
+
 from yt_napari.schemas import _manager
 
 
@@ -11,8 +13,8 @@ class InputModel(BaseModel):
     field_type: str
     field_name: str
 
-    left_edge: Optional[Tuple[float, float, float]] = [0., 0., 0.]
-    right_edge: Optional[Tuple[float, float, float]] = [1., 1., 1.]
+    left_edge: Optional[Tuple[float, float, float]] = [0.0, 0.0, 0.0]
+    right_edge: Optional[Tuple[float, float, float]] = [1.0, 1.0, 1.0]
     edge_units: Optional[str] = "code_length"
     resolution: Optional[Tuple[int, int, int]] = [400, 400, 400]
     take_log: Optional[bool] = True
@@ -20,19 +22,11 @@ class InputModel(BaseModel):
     _schema_prefix = "yt-napari"
 
 
-def _store_schema(schema_db: Optional[Union[PosixPath, str]]=None,
-                  **kwargs):
+def _store_schema(schema_db: Optional[Union[PosixPath, str]] = None, **kwargs):
     # save the current data model as a new schema
     if schema_db is None:
         schema_db = PosixPath(inspect.getfile(_manager)).parent
     m = _manager.Manager(schema_db)
     prefix = InputModel._schema_prefix
-    m.write_new_schema(InputModel.schema_json(indent=2),
-                       schema_prefix=prefix,
-                       **kwargs)
-
-
-
-
-
-
+    schema_contents = InputModel.schema_json(indent=2)
+    m.write_new_schema(schema_contents, schema_prefix=prefix, **kwargs)
