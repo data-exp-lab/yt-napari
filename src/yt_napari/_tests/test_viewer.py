9090a8973c96859dadd4ada10ee19b32dbe3cec8
Chris Havlin <chris.havlin@gmail.com>
2022-03-09 13:22:04 -0800

    use the make_napari_viewer fixture

--- src/yt_napari/_tests/test_viewer.py
+++ src/yt_napari/_tests/test_viewer.py
@@ -1,40 +1,36 @@
-import napari
 import yt
 
 from yt_napari.viewer import Scene
 
 
-def test_viewer():
+def test_viewer(make_napari_viewer):
 
-    i_am_local = False
+    # make_napari_viewer is a pytest fixture. It takes any keyword arguments
+    # that napari.Viewer() takes. The fixture takes care of teardown, do **not**
+    # explicitly close it!
+    viewer = make_napari_viewer()
 
-    if i_am_local:
-        # the following is some initial testing, but need to modify to work in a
-        # headless state.
-        nap_view = napari.Viewer()
-        sc = Scene()
-        ds = yt.testing.fake_amr_ds(fields=("density", "mass"), units=("kg/m**3", "kg"))
-        sc.add_to_viewer(nap_view, ds, ("gas", "density"))
+    sc = Scene()
+    ds = yt.testing.fake_amr_ds(fields=("density", "mass"), units=("kg/m**3", "kg"))
+    sc.add_to_viewer(viewer, ds, ("gas", "density"))
 
-        expected_layers = 1
-        assert len(nap_view.layers) == expected_layers
+    expected_layers = 1
+    assert len(viewer.layers) == expected_layers
 
-        # hmm, why doesnt this pass the test. warning gets raised, pytest fails
-        # with pytest.warns(RuntimeWarning):
-        #     sc.add_to_viewer(nap_view, ds, [("gas", "density")], translate=10)
-        #
-        # with pytest.warns(RuntimeWarning):
-        #     sc.add_to_viewer(nap_view, ds, [("gas", "density")], scale=10)
-        # assert len(nap_view.layers) == 4
+    # hmm, why doesnt this pass the test. warning gets raised, pytest fails
+    # with pytest.warns(RuntimeWarning):
+    #     sc.add_to_viewer(nap_view, ds, [("gas", "density")], translate=10)
+    #
+    # with pytest.warns(RuntimeWarning):
+    #     sc.add_to_viewer(nap_view, ds, [("gas", "density")], scale=10)
+    # assert len(nap_view.layers) == 4
 
-        sc.add_to_viewer(nap_view, ds, ("gas", "density"))
-        expected_layers += 1
-        assert len(nap_view.layers) == expected_layers
+    sc.add_to_viewer(viewer, ds, ("gas", "density"))
+    expected_layers += 1
+    assert len(viewer.layers) == expected_layers
 
-        # build a new scene so it builds from prior
-        sc = Scene()
-        sc.add_to_viewer(nap_view, ds, ("gas", "density"))
-        expected_layers += 1
-        assert len(nap_view.layers) == expected_layers
-
-        nap_view.close()
+    # build a new scene so it builds from prior
+    sc = Scene()
+    sc.add_to_viewer(viewer, ds, ("gas", "density"))
+    expected_layers += 1
+    assert len(viewer.layers) == expected_layers
