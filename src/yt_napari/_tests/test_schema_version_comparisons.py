51552a30f7c2078c255e6d23bf78d89639f630dc
chrishavlin <chris.havlin@gmail.com>
2023-08-02 11:03:12 -0500

    adjust one more test...

--- src/yt_napari/_tests/test_schema_version_comparisons.py
+++ src/yt_napari/_tests/test_schema_version_comparisons.py
@@ -1,6 +1,5 @@
 import pytest
 
-from yt_napari._version import version_tuple
 from yt_napari.schemas import _version_comparison as vc
 
 
@@ -10,7 +9,7 @@ from yt_napari.schemas import _version_comparison as vc
         ("yt-napari_0.1.3.json", (0, 1, 3)),
         ("yt-napari_2.1.0.json", (2, 1, 0)),
         ("/blah/blah/yt-napari_2.1.0.json", (2, 1, 0)),
-        ("/blah/blah/yt-napari_latest.json", version_tuple[:3]),
+        ("/blah/blah/yt-napari_latest.json", vc._get_version_tuple()),
     ],
 )
 def test_version_tupling(string_to_test, expected):
