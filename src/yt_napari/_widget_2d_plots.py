a7f0defe9be2fb218eb55dcf9e8807e32a051ba7
Chris Havlin <chris.havlin@gmail.com>
2024-11-21 14:45:10 -0600

    initial widget works

--- src/yt_napari/_widget_2d_plots.py
+++ src/yt_napari/_widget_2d_plots.py
@@ -1,8 +1,11 @@
 import napari
-import numpy as np
 import yt
 from magicgui import widgets
-from qtpy.QtWidgets import QComboBox, QHBoxLayout, QVBoxLayout, QWidget
+from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg
+from qtpy.QtWidgets import QComboBox, QLabel, QVBoxLayout, QWidget
+
+from yt_napari._gui_utilities import clearLayout
+from yt_napari.viewer import layers_to_yt
 
 
 class YTPhasePlot(QWidget):
@@ -23,20 +26,35 @@ class YTPhasePlot(QWidget):
             value=active_layers[0], choices=active_layers, name="Layer 3"
         ).native
 
+        self.layer_4_weight: QComboBox = widgets.ComboBox(
+            value="None",
+            choices=[
+                "None",
+            ]
+            + active_layers,
+            name="Layer 4",
+        ).native
+
         self.layout().addWidget(self.layer_1)
         self.layout().addWidget(self.layer_2)
         self.layout().addWidget(self.layer_3)
+        self.layout().addWidget(self.layer_4_weight)
 
         update_layers = widgets.PushButton(text="Refresh layers")
         update_layers.clicked.connect(self.update_available_layers)
         self.update_layers = update_layers
         self.layout().addWidget(self.update_layers.native)
 
-        self.phaseplot_container = QHBoxLayout()
         self.render_button = widgets.PushButton(text="Render")
         self.render_button.clicked.connect(self.render_phaseplot)
-        self.phaseplot_container.addWidget(self.render_button.native)
+        self.layout().addWidget(self.render_button.native)
+
+        self.phaseplot_container = QVBoxLayout()
+        self.phaseplot_container.addWidget(QLabel(text="Click render to generate plot"))
         self.layout().addLayout(self.phaseplot_container)
+
+        self.fig = None
+        self.canvas = None
 
     def update_available_layers(self):
         print("update those layers")
@@ -53,45 +71,64 @@ class YTPhasePlot(QWidget):
         self.layer_3.clear()
         self.layer_3.addItems(layers)
         self.layer_3.setCurrentIndex(0)
-
-        # add option for weight_field
+        # weight field
+        self.layer_4.clear()
+        self.layer_4.addItems(
+            [
+                "None",
+            ]
+            + layers
+        )
+        self.layer_4.setCurrentIndex(0)
 
     def render_phaseplot(self):
 
         layer1 = self.current_layers[self.layer_1.currentIndex()]
         layer2 = self.current_layers[self.layer_2.currentIndex()]
         layer3 = self.current_layers[self.layer_3.currentIndex()]
+        wt_field = self.current_layers[self.layer_4_weight.currentIndex()]
+        if wt_field == "None":
+            wt_field = None
 
-        print("get those layers")
+        layers = [layer1, layer2, layer3, wt_field]
+        layers_for_yt = []
+        pp_args = []
+        for layer in layers:
+            print(layer)
+            if _is_index_field(layer):
+                pp_args.append(("index", layer))
+            elif layer is not None:
+                pp_args.append(("stream", layer))
+                layers_for_yt.append(layer)
+            else:
+                pp_args.append(None)
 
-        l1 = self.viewer.layers[layer1]
-        l2 = self.viewer.layers[layer2]
-        l3 = self.viewer.layers[layer3]
+        ds = layers_to_yt(self.viewer, layers_for_yt, axis_order=("z", "y", "x"))
 
-        data = {
-            l1.name: l1.data,
-            l2.name: l2.data,
-            l3.name: l3.data,
-        }
-
-        dims = data[l1.name].shape
-        bbox = np.array([[0, dims[idim]] for idim in range(len(dims))])
-        ds = yt.load_uniform_grid(data, dims, bbox=bbox, length_unit=1)
-        # TODO: adjust for when selecting x, y, z
         pp = yt.PhasePlot(
             ds,
-            ("stream", l1.name),
-            ("stream", l2.name),
-            ("stream", l3.name),
-            weight_field=None,
+            pp_args[0],
+            pp_args[1],
+            pp_args[2],
+            weight_field=pp_args[3],
         )
         pp.render()
-        print("rendered... need to put the image in the widget...")
+
+        clearLayout(self.phaseplot_container)
+
+        self.fig = pp.plots[pp.fields[0]].figure
+        self.canvas = FigureCanvasQTAgg(self.fig)
+        self.canvas.draw()
+        self.phaseplot_container.addWidget(self.canvas)
 
     @property
     def available_layer_list(self):
         layers = [layer.name for layer in self.viewer.layers]
 
-        dims = ["x", "y", "z"]  # TODO: check ndim
+        dims = ["x", "y", "z", "ones"]  # TODO: check ndim
         layers += dims
         return layers
+
+
+def _is_index_field(layer_name: str):
+    return layer_name in ("x", "y", "z", "ones")
